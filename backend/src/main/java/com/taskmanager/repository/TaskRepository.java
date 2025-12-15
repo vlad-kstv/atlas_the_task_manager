d50b0e42cdd034b9b10ea9d85bd949636333fb84
Vlad Kustov <vlad.kustov07@gmail.com>
2025-12-15 11:47:39 +0100

    init repositories

---
+++ backend/src/main/java/com/taskmanager/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.taskmanager.repository;
+
+import com.taskmanager.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
