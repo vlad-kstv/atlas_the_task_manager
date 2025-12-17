98b391ad5f0e9a64d795cb9b8537b66f403088bd
Vlad <vlad.kustov07@gmail.com>
2025-12-17 12:43:49 +0100

    project membership entity

---
+++ backend/src/main/java/com/taskmanager/entity/ProjectMembership.java
@@ -0,0 +1,19 @@
+package com.taskmanager.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity(name = "project_memberships")
+public class ProjectMembership {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User member;
+}
