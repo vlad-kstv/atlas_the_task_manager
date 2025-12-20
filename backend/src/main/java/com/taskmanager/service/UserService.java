f8819ceb0b4e2fb6d3ee9140bd169af30b894207
Vlad <vlad.kustov07@gmail.com>
2025-12-20 23:58:13 +0100

    user service upd

---
+++ backend/src/main/java/com/taskmanager/service/UserService.java
@@ -0,0 +1,16 @@
+package com.taskmanager.service;
+
+import com.taskmanager.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository repository;
+
+    @Autowired
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+}
