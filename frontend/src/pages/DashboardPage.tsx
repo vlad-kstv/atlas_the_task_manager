2aedcf2b2040df03d5ca8a29eb4c96f62cdd0b87
Vlad <vlad.kustov07@gmail.com>
2026-01-22 11:20:56 +0100

    switching to shadcn

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import Navbar from '../components/layout/Navbar';
 import { Card, Layout, Flex } from 'antd';
 import { ProjectService } from '../api/projectService';
+import { Button } from '@/components/ui/button';
 
 const { Sider, Content } = Layout;
 
@@ -22,6 +23,7 @@ export default function DashboardPage() {
         <h1>Recent</h1>
         <Content style={{ padding: '24px', background: '#f5f5f5' }}>
           <Flex gap="middle" wrap="wrap">
+            <Button>Test</Button>
             <Card title="Default size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
               <p>Card content</p>
             </Card>
