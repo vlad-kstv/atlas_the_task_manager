7da982aee1bb74bddc4a710990db7bca4b7b7a0c
Vlad <vlad.kustov07@gmail.com>
2026-02-03 10:10:45 +0100

    changed return value to ResponseEntity

--- backend/src/main/java/com/taskmanager/controller/ProjectRestController.java
+++ backend/src/main/java/com/taskmanager/controller/ProjectRestController.java
@@ -5,6 +5,7 @@ import com.taskmanager.dto.ProjectResponseDto;
 import com.taskmanager.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,12 +19,18 @@ public class ProjectRestController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ProjectResponseDto createProject(@RequestBody ProjectRequestDto dto){
-        return service.createProject(dto);
+    public ResponseEntity<ProjectResponseDto> createProject(@RequestBody ProjectRequestDto dto){
+        ProjectResponseDto response = service.createProject(dto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
     }
 
     @GetMapping("/users/{userId}/projects")
-    public List<ProjectResponseDto> getUserProjects(@PathVariable Long userId) {
-        return service.getUserProject(userId);
+    public ResponseEntity<List<ProjectResponseDto>> getUserProjects(@PathVariable Long userId) {
+        List<ProjectResponseDto> response = service.getUserProject(userId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
     }
 }
